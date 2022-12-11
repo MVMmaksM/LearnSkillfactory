5496bf3d6de9e4b2fc2d899e3c158f9f055f0f0c
MVMmaksM <vaganov.maksim@mail.ru>
2022-12-11 20:39:13 +0500

    Вывод двумерного массива по столбцам

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -1,35 +1,60 @@
-﻿namespace Module4
+﻿using System;
+
+namespace Module4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            Console.WriteLine("Введите ваше имя:");
 
-            var color = Console.ReadLine();
+            int[,] array = new int [4,3]{ { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-            if (color == "red")
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                    Console.Write(array[k, i] + " ");
 
-                Console.WriteLine("Your color is red!");
+                Console.WriteLine();
             }
 
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+            //Console.Write($"Последняя буква вашего имени {name[name.Length - 1]}");
 
-                Console.WriteLine("Your color is cyan!");
-            }
+            //Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            //var color = Console.ReadLine();           
+
+
+            //switch (color)
+            //{
+            //    case "red":
+            //        Console.BackgroundColor = ConsoleColor.Red;
+            //        Console.ForegroundColor = ConsoleColor.Black;
+
+            //        Console.WriteLine("Your color is red!");
+            //        break;
+
+            //    case "green":
+            //        Console.BackgroundColor = ConsoleColor.Green;
+            //        Console.ForegroundColor = ConsoleColor.Black;
+
+            //        Console.WriteLine("Your color is green!");
+            //        break;
+
+            //    case "cyan":
+            //        Console.BackgroundColor = ConsoleColor.Cyan;
+            //        Console.ForegroundColor = ConsoleColor.Black;
+            //        break;
+
+            //    default:
+            //        Console.BackgroundColor = ConsoleColor.Yellow;
+            //        Console.ForegroundColor = ConsoleColor.Red;
+
+            //        Console.WriteLine("Your color is yellow!");
+            //        break;
+            //}
+
         }
     }
 }
