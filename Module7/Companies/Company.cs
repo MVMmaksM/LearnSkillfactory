d2672fab693d033fa15094149c78d7c94cd64973
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-11 16:37:18 +0500

    Create Delivery and Company

---
+++ Module7/Companies/Company.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7.Companies
+{
+    internal abstract class Company
+    {
+        public string? Name { get;}
+        public virtual string? Description { get;}
+        public virtual string? Address { get;}
+        public virtual string? PhoneNumber { get;}
+        public virtual string? Email { get; }
+
+        public Company(string name, string address, string phoneNumber, string description = "Неизвестно", string email = "Неизвестно")
+        {
+            Name = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Description = description;
+            Email = email;
+        }
+
+        public virtual void ShowInfoCompany() 
+        {
+            Console.WriteLine($"Наименование организации: {Name}\nАдрес: {Address}\nНомер телефона: {PhoneNumber}\nEmail: {Email}\nОписание: {Description}");
+        }
+    }
+}
---
+++ Module7/Deliveries/Delivery.cs
@@ -0,0 +1,17 @@
+﻿using Module7.Companies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7.Deliveries
+{
+    internal abstract class Delivery<TCompany> where TCompany: Company
+    {
+        public string Name { get; set; }
+        public TCompany Company { get; set; }
+        public string Description { get; set; }
+        public abstract void ShowInfoDelivery();
+    }
+}
