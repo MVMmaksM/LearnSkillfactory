dd1f29d5856ad102e6636a8738651dbb996ba275
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-12 09:22:55 +0500

    Create inexator

---
+++ Module7/Orders/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module7.Deliveries;
+using Module7.Companies;
+using Module7.Products;
+using Module7.Manufacturers;
+
+namespace Module7.Orders
+{
+    internal class Order<TDelivery, TId, TProduct> 
+    {
+        private List<TProduct> _products;
+        public DateTime DateCreate { get;}             
+        public TId? ID { get;}
+        public TDelivery? Delivery { get; set;}
+        public Order(TDelivery delivery, List<TProduct> products, TId id)
+        {
+            ID = id;
+            DateCreate = DateTime.Now;
+            Delivery = delivery;
+            _products = products;
+        }
+
+        public TProduct this[int index]
+        { 
+            get 
+            {
+                if (index>=0&&index<_products.Count)
+                {
+                    return _products[index];
+                }
+                else
+                {
+                    return default(TProduct);
+                }
+            }
+            private set
+            {
+                if (index >= 0 && index < _products.Count)
+                {
+                    _products[index] = value;
+                }
+            }
+        }
+    }
+}
