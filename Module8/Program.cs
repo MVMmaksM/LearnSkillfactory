e6e066e620cb35bd740725aadacef30e07e5e8fd
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-16 15:15:15 +0500

    Module9

---
+++ Module8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace Module8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }       
+    }
+}
---
+++ Module9/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Module9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Diff;
+
+            Console.WriteLine(myDelegate(10, 15));
+        }
+
+        delegate int MyDelegate(int a, int b);
+        public static int Diff(int a, int b) => a - b; 
+    }
+}
