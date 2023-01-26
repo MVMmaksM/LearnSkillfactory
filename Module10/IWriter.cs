3f01ed83c3a86ebe57aa3cf4a500de5c8f9414bb
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-26 14:09:16 +0500

    Create module 10

---
+++ Module10/IWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    internal interface IWriter
+    {
+        public int MyProperty { get; set; }
+        void Write() => Console.WriteLine("Hello");
+    }
+}
---
+++ Module10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWriter writer = new Writer();
+            writer.Write();
+        }
+    }
+}
