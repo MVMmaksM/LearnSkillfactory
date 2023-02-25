5d9cd6a03f239a40289641ffced92c96bcf1ecbc
Maks <Maks@DESKTOP-6KU2K6N>
2023-02-25 18:53:08 +0500

    Module13

---
+++ Module13/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13
+{
+    internal class Contact
+    {
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Module13/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Module13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите текст:");
+                var text = Console.ReadLine();
+
+                var sing = new char[] { ' ', '.', ',' };
+                var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                
+                var chars = text?.ToCharArray();
+               
+                HashSet<char> set = new HashSet<char>(chars);
+
+                bool isNumbers = set.Overlaps(numbers);
+                Console.WriteLine($"Коллекция содержит цифры: {isNumbers}");
+
+                set.ExceptWith(sing);
+                Console.WriteLine($"Длина коллекции: {set.Count}\n");
+            }                    
+        }
+
+        public static List<string> GetMissing(List<string> month, ArrayList missing)
+        {
+            var result = new List<string>();
+
+            foreach (var miss in missing)
+            {
+                if ((miss is string) && (!month.Contains(miss)))
+                {
+                    result.Add((string)miss);
+                }
+            }
+
+            foreach (var item in result)
+            {
+                missing.Remove(item);
+            }
+
+            month.AddRange(result);
+
+            return month;
+        }
+        public static void AddContact(List<Contact> contacts, Contact contact)
+        {
+            bool isExist = false;
+
+            foreach (var con in contacts)
+            {
+                if (con.Name == contact.Name)
+                {
+                    isExist = true;
+                }
+            }
+
+            if (!isExist)
+            {
+                contacts.Add(contact);
+            }
+        }
+
+        static bool isSortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i + 1] <= array[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static ArrayList NewArrayList(ArrayList input)
+        {
+            int sum = default(int);
+            StringBuilder text = new StringBuilder();
+            var outputArrList = new ArrayList();
+
+            foreach (var value in input)
+            {
+                if (value is int)
+                {
+                    sum += (int)value;
+                }
+                else if (value is string)
+                {
+                    text.Append(value);
+                }
+            }
+
+            outputArrList.Add(sum);
+            outputArrList.Add(text);
+
+            return outputArrList;
+        }
+    }
+}
