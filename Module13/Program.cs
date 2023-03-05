e8e036cd7333b32adb443503078554043325a0d6
Maks <Maks@DESKTOP-6KU2K6N>
2023-03-05 15:05:47 +0500

    Create Task 13.6.1

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -7,37 +7,9 @@ namespace Module13
 {
     internal class Program
     {
-        private static Dictionary<string, Contact> PhoneBook = new Dictionary<string, Contact>()
-        {
-            ["Макс"] = new Contact(89003795411, "vaganov.maksim@mail.ru"),
-            ["Иван"] = new Contact(89005548988, "vaganov.maksim@mail.ru"),
-        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Список контаков:");
-            ShowContacts();
-
-            var isAddContact = PhoneBook.TryAdd("Дмитрий", new Contact(89366655545, "@email.ru"));
-            Console.WriteLine($"\nКонтакт добавлен: {isAddContact}");
-            ShowContacts();
-
-            if (PhoneBook.TryGetValue("Дмитрий", out Contact? contact))
-            {
-                contact.PhoneNumber = 8900000000;
-                Console.WriteLine("Контакт изменен");
-            }
-
-            Console.WriteLine("\nСписок с изсененным контактом");
-            ShowContacts();
-
-        }
-
-        public static void ShowContacts() 
-        {
-            foreach (var contact in PhoneBook)
-            {
-                Console.WriteLine($"Имя: {contact.Key} телефон: {contact.Value.PhoneNumber} Email: {contact.Value.Email}");
-            }
+                        
         }
     }
 }
---
+++ Task13.6.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Xml;
+
+namespace Task13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFile = @"C:\Users\Maks\Downloads\Text1.txt";
+            var textFile = ReadFile(pathFile);
+
+            if (string.IsNullOrEmpty(textFile))
+            {
+                return;
+            }
+
+            var arraySplit = textFile.Split(' ');
+
+            var stopWatchList = Stopwatch.StartNew();
+
+            List<string> listWords = new List<string>();
+            //listWords.AddRange(arraySplit);
+
+            foreach (var word in arraySplit)
+            {
+                listWords.Add(word);
+            }
+
+            Console.WriteLine($"Добавление в List<string>: {stopWatchList.Elapsed.TotalMilliseconds}");
+
+            var stopWatchLinked = Stopwatch.StartNew();
+
+            LinkedList<string> linkedWords = new LinkedList<string>();
+            foreach (var word in arraySplit)
+            {
+                linkedWords.AddFirst(word);
+            }
+
+            Console.WriteLine($"Добавление в LinkedList<string>: {stopWatchLinked.Elapsed.TotalMilliseconds}");
+        }
+
+        static string ReadFile(string pathFile)
+        {
+            string textFile = string.Empty;
+
+            try
+            {
+                if (File.Exists(pathFile))
+                {
+                    using (StreamReader streamReader = new StreamReader(pathFile))
+                    {
+                        textFile = streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return textFile;
+        }
+    }
+}
