72a2320841647ba613f54bdfd878b94adcc4398e
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-03-07 11:34:41 +0500

    unit 14.1

---
+++ Module14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+namespace Module14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+
+            var selectStudents = students.SelectMany(
+                s => s.Languages, (s, l) => new { Student = s, Lang = l });
+
+            foreach (var s in selectStudents)
+            {
+                Console.WriteLine($"Имя студента: {s.Student.Name}");
+                Console.WriteLine($"Язык студента: {s.Lang}\n");
+            }
+
+        }                
+    }
+}
---
+++ Module14/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module14
+{
+    internal class Student
+    {
+        public string? Name { get; set; }
+        public byte Age { get; set; }
+        public List<string>? Languages { get; set; }
+    }
+}
