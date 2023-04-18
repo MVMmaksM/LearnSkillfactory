c6a65f673df3ba87525e9b30d48c8aa3b429d8bd
Maks <Maks@DESKTOP-6KU2K6N>
2023-04-18 13:29:28 +0500

    create interface IAccount and class Account

---
+++ Task17.6.4/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17._6._4
+{
+    internal class Account : IAccount
+    {
+        public string? Type { get; set; }
+        public double Balance { get; set; }
+        public double Interes { get; set; }
+    }
+}
---
+++ Task17.6.4/IAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17._6._4
+{
+    internal interface IAccount
+    {
+        // тип учетной записи
+        string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+        
+        // процентная ставка
+        public double Interes { get; set; }
+    }
+}
