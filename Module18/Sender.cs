d807ad6a343038fdc26e0b67308cf5c82414fb21
Maks <Maks@DESKTOP-6KU2K6N>
2023-04-18 22:10:45 +0500

    Create class sender

---
+++ Module18/Sender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18
+{
+    internal class Sender
+    {
+        ICommand command;       
+        public void SetCommand(ICommand command) => this.command = command;
+        public void LoadVideo(string urlVideo, string outputFilePath) => command.LoadVideo(urlVideo, outputFilePath);
+        public void GetInfo(string urlVideo)=> command.GetInfo(urlVideo);
+    }
+}
